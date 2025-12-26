b73d3841a14b74ae4df77f5d4b8832d4fa80ad10
Alec Deschene <alecdeschene@gmail.com>
2025-12-26 01:31:00 -0800

    Added style class for imagebuttons

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import tkinter as tk
 from tkinter import ttk
+
+from matplotlib import style
 from gui.main_window import MainWindow
 
 def main():
     root = tk.Tk()
     
     # Import the tcl file
-    root.tk.call('source', 'forest-dark.tcl')
+    root.tk.call('source', 'assets/forest-dark.tcl')
     # Set the theme with the theme_use method
     style = ttk.Style(root)
     style.theme_use('forest-dark')
     # Treeview style config
     style.configure("Treeview", rowheight=30)
+    # Create a custom style that removes all internal padding
+    style.configure("Nopadding.TButton", padding=0)
 
     app = MainWindow(root)
 
